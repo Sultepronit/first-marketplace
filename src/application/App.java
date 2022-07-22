f793f83af5400ca2acaeb1065cb2448e7f043445
Sultepronit <sultepronit@gmail.com>
2022-07-22 11:00:54 +0300

    Added proper greeting message

--- src/application/App.java
+++ src/application/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello marketplace!");
+		System.out.println("Hello! This marketplace app have no GUI, but just follow simple instructions.");
 
 	}
 
